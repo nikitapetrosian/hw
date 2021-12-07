57a408b690eec212076df3ec3872e4a94a25489d
Nikita <nekitrus92@yandex.ru>
2021-12-07 16:33:58 +0300

    login

---
+++ src/app/hooks/useAuth.jsx
@@ -0,0 +1,94 @@
+import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import userService from "../services/user.service";
+import { toast } from "react-toastify";
+import { setTokens } from "../services/localStorage.service";
+
+const httpAuth = axios.create();
+const AuthContext = React.createContext();
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+const AuthProvider = ({ children }) => {
+    const [currentUser, setUser] = useState({});
+    const [error, setError] = useState(null);
+
+    async function signIn({ email, password }) {
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        let errorObject = { error: "" };
+        try {
+            const { data } = await httpAuth.post(url, { email, password, returnSecureToken: true });
+            setTokens(data);
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            if (code !== 200) {
+                errorObject.error = message;
+                if (message === "INVALID_PASSWORD" || message === "EMAIL_NOT_FOUND") {
+                    errorObject = { error: "че-то не так" };
+                };
+            };
+        }
+        return new Promise(function (resolve, reject) {
+            if (errorObject.error) {
+                reject(errorObject);
+            } else {
+                resolve();
+            }
+        });
+    };
+
+    async function signUp({ email, password, ...rest }) {
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+        try {
+            const { data } = await httpAuth.post(url, { email, password, returnSecureToken: true });
+            setTokens(data);
+            await createUser({ _id: data.localId, email, ...rest });
+            console.log(data);
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                if (message === "EMAIL_EXISTS") {
+                    const errorObject = { email: "Пользователь с таким Email уже есть" };
+                    throw errorObject;
+                };
+            };
+        }
+    };
+    async function createUser(data) {
+        try {
+            const { content } = await userService.create(data);
+            setUser(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    };
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+    return (
+        <AuthContext.Provider value={{ signUp, signIn, currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
+export default AuthProvider;
---
+++ src/app/services/localStorage.service.js
@@ -0,0 +1,28 @@
+
+const TOKEN_KEY = "jwt-token";
+const REFRESH_KEY = "jwt-refresh-token";
+const EXPIRES_KEY = "jwt-expires";
+
+export function setTokens({ refreshToken, expiresIn = 3600, idToken }) {
+    const expiresData = new Date().getTime() + expiresIn * 1000;
+    localStorage.setItem(TOKEN_KEY, idToken);
+    localStorage.setItem(REFRESH_KEY, refreshToken);
+    localStorage.setItem(EXPIRES_KEY, expiresData);
+};
+export function getAccessToken() {
+   return localStorage.getItem(TOKEN_KEY);
+};
+export function getRefreshToken() {
+    return localStorage.getItem(REFRESH_KEY);
+ };
+ export function getTokenExpiresData() {
+    return localStorage.getItem(EXPIRES_KEY);
+ };
+
+const localStorageService = {
+    setTokens,
+    getAccessToken,
+    getRefreshToken,
+    getTokenExpiresData
+};
+export default localStorageService;
